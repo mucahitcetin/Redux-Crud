5c7759a601f66755fdbc3a63fd5e3a25698615ae
mucahitcetin <mucahitcetin01@gmail.com>
2024-05-05 15:30:08 +0300

    first commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+import { NavLink } from "react-router-dom";
+import { toggleTheme } from "../redux/slices/counterSlice";
+import { useDispatch } from "react-redux";
+
+const Header = () => {
+  const dispatch = useDispatch();
+
+  return (
+    <header className="mb-3 p-4">
+      <div className="container d-flex justify-content-between">
+        <h2>TOOLKIT</h2>
+
+        <nav className="d-flex  gap-3">
+          <NavLink to="/">Sayaç</NavLink>
+          <NavLink to="/crud">CRUD</NavLink>
+
+          <button onClick={() => dispatch(toggleTheme())}>Tema Değiş</button>
+        </nav>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
---
+++ src/redux/slices/counterSlice.js
@@ -0,0 +1,45 @@
+/*
+ * createSlice methodu import edilir
+ * createSlice'in istedği parametreler
+ * - - name: slice ismi > string
+ * - - initialState: başlangıç state'i
+ * - - reducers: aksiyonların görevlerini tanımladığımız fonksiyonlar
+ */
+
+import { createSlice } from "@reduxjs/toolkit";
+
+const counterSlice = createSlice({
+  // Slice'in adı
+  name: "counter",
+  // Başlangıç state'i, number türünde olmalıdır.
+  initialState: { count: 0, isDarkTheme: true },
+  // state'in nasıl değişiceğine karar veren fonksiyonları tanımalama
+  reducers: {
+    // 1.aksiyon
+    increase: (state) => {
+      state.count++;
+    },
+
+    // 2.aksiyon
+    decrease: (state) => {
+      state.count--;
+    },
+
+    // 3.aksiyon
+    setCount: (state, action) => {
+      state.count = action.payload;
+    },
+
+    // 4.aksiyon
+    toggleTheme: (state) => {
+      state.isDarkTheme = !state.isDarkTheme;
+    },
+  },
+});
+
+// slice'İn oluşturduğu reducer fonksiyonunu store'a tanıtmak için export ederiz
+export default counterSlice.reducer;
+
+// sliice'in oluşturduğu aksiyon oluşturan fonksiyonları bileşenlerde kullanabilmek için export ederiz
+export const { decrease, increase, setCount, toggleTheme } =
+  counterSlice.actions;
